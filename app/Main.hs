3e5c204ae788e371eeaea6af76c4663a7fe34bd1
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2016-08-12 13:48:28 +0500

    Make minimal gloss app: black screen

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,16 @@
 module Main where
 
 import Snakes
+import Graphics.Gloss.Interface.Pure.Game
 
 main :: IO ()
-main = someFunc
+main = play display bgColor fps initialWorld renderWorld handleWorld updateWorld
+  where
+    display = InWindow "The Game of Snakes" (500, 500) (200, 200)
+    bgColor = black
+    fps     = 30
+
+    initialWorld = ()
+    renderWorld   w = blank
+    handleWorld _ w = w
+    updateWorld _ w = w
