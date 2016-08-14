3b9ca8fa2be2146a5edf9c306f63765cd9a57c92
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2016-08-14 22:01:31 +0500

    Check objects collision

--- src/Snakes/Model/Universe.hs
+++ src/Snakes/Model/Universe.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TupleSections #-}
 module Snakes.Model.Universe where
 
 import Control.Applicative
+import Control.Monad
 import Data.Traversable
 import Graphics.Gloss
 
@@ -21,10 +23,33 @@ initUniverse ps = Universe
   <*> traverse mkFood ps
 
 updateUniverse :: Float -> Universe -> GameConfig -> Universe
-updateUniverse dt u@Universe{..} cfg = u
+updateUniverse dt
+    = updateUniverseObjects dt
+  >=> checkFoodCollision
+  >=> checkSnakeCollision
+
+updateUniverseObjects :: Float -> Universe -> GameConfig -> Universe
+updateUniverseObjects dt u@Universe{..} cfg = u
   { uSnake = moveSnake dt uSnake cfg
   , uFood  = newFood }
   where
     newFood = case updateFood dt (head uFood) of
       Nothing -> tail uFood
       Just f  -> f : tail uFood
+
+checkFoodCollision :: Universe -> GameConfig -> Universe
+checkFoodCollision u@Universe{..} GameConfig{..}
+  | (head (snakeLinks uSnake), linkSize) `collides` (foodLocation (head uFood), foodSize)
+    = u { uFood = tail uFood }
+  | otherwise = u
+
+checkSnakeCollision :: Universe -> GameConfig -> Universe
+checkSnakeCollision u@Universe{..} GameConfig{..}
+  | any (collides (head (snakeLinks uSnake), linkSize)) (map (, foodSize) (drop 2 (snakeLinks uSnake)))
+    = u
+  | otherwise = u
+
+-- | Check collision for two objects.
+-- Collision counts when objects' are at least halfway into each other.
+collides :: (Point, Float) -> (Point, Float) -> Bool
+collides ((x, y), a) ((u, v), b) = ((a + b) / 2)^2 > (x - u)^2 + (y - v)^2
