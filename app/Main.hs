baf3fb947dd26530b64de437abd27de6aa061556
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2016-08-15 13:24:36 +0500

    Wrap Universe in a TVar

--- app/Main.hs
+++ app/Main.hs
@@ -1,24 +1,37 @@
 {-# LANGUAGE RecordWildCards #-}
 module Main where
 
-import Graphics.Gloss.Interface.Pure.Game
+import Control.Concurrent.STM
+import Graphics.Gloss.Interface.IO.Game
+import System.Exit
+
 import Snakes
 
 main :: IO ()
 main = do
   u <- randomUniverse cfg
-  let initialWorld = addPlayer "You" u cfg
-  play display bgColor fps initialWorld renderWorld handleWorld updateWorld
+  initialWorld <- atomically $ newTVar (addPlayer "You" u cfg)
+  playIO display bgColor fps initialWorld renderWorld handleWorld updateWorld
   where
     display = InWindow "The Game of Snakes" (w, h) (100, 100)
     bgColor = black
     fps     = 60
 
-    renderWorld  = flip renderUniverse cfg
-    updateWorld dt = flip (updateUniverse dt) cfg
+    renderWorld w = do
+      u <- readTVarIO w
+      return (renderUniverse u cfg)
 
-    handleWorld (EventMotion mouse) = flip (handlePlayerAction "You" (RedirectSnake mouse)) cfg
-    handleWorld _ = id
+    updateWorld dt w = atomically $ do
+      u <- readTVar w
+      writeTVar w (updateUniverse dt u cfg)
+      return w
+
+    handleWorld (EventKey (SpecialKey KeyEsc) Down _ _) _ = exitSuccess -- exit on ESC
+    handleWorld (EventMotion mouse) w = atomically $ do
+      u <- readTVar w
+      writeTVar w (handlePlayerAction "You" (RedirectSnake mouse) u cfg)
+      return w
+    handleWorld _ w = return w
 
     cfg@GameConfig{..} = defaultGameConfig
     (fieldWidth, fieldHeight) = fieldSize
