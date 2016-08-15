43adda0b2fb93a5d5b2f80eb96bd22f7862de9c8
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2016-08-15 19:07:16 +0500

    Add two more bots

--- src/Snakes/Bot.hs
+++ src/Snakes/Bot.hs
@@ -9,3 +9,10 @@ type Bot = Universe -> Maybe Action
 simpleBot :: Bot
 simpleBot Universe{..} = Just (RedirectSnake (foodLocation (head uFood)))
 
+phantomBot :: Bot
+phantomBot Universe{..}
+  | bonusEffect (head uBonuses) == BonusPhantom = Just (RedirectSnake (bonusLocation (head uBonuses)))
+  | otherwise = Just (RedirectSnake (foodLocation (head uFood)))
+
+bonusBot :: Bot
+bonusBot Universe{..} = Just (RedirectSnake (bonusLocation (head uBonuses)))
